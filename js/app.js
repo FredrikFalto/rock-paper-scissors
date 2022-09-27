4ec2fe95db026563eb318a486eccde10d0afee62
FredrikFalto <faltofredrik@gmail.com>
2022-09-27 14:02:39 +0200

    Solution-1 done

--- js/app.js
+++ js/app.js
@@ -12,10 +12,69 @@ selections.forEach((selection) => {
 
         // console.log(pInput, '-', cInput);
         updateMoveImgs(pInput, cInput);
+        compareInputs(pInput, cInput);
+        updateScore();
+
+        if (checkWin()) {
+            pScore = cScore = 0;
+
+            updateScore();
+        }
     });
 });
 
 function updateMoveImgs(pInput, cInput) {
     document.getElementById('p-move').src = `./img/${pInput}.svg`;
     document.getElementById('c-move').src = `./img/${cInput}.svg`;
+}
+
+function compareInputs(pInput, cInput) {
+    if (pInput === cInput) {
+        document.getElementById('result-text').innerHTML = 'It is a tie!';
+        return;
+    }
+
+    if (pInput === 'rock') {
+        if (cInput === 'scissors') {
+            document.getElementById('result-text').innerHTML = 'You win!';
+            pScore++;
+        } else {
+            document.getElementById('result-text').innerHTML = 'Computer wins!';
+            cScore++;
+        }
+    } else if (pInput === 'paper') {
+        if (cInput === 'rock') {
+            document.getElementById('result-text').innerHTML = 'You win!';
+            pScore++;
+        } else {
+            document.getElementById('result-text').innerHTML = 'Computer wins!';
+            cScore++;
+        }
+    } /* pInput == "scissors" */ else {
+        if (cInput === 'paper') {
+            document.getElementById('result-text').innerHTML = 'You win!';
+            pScore++;
+        } else {
+            document.getElementById('result-text').innerHTML = 'Computer wins!';
+            cScore++;
+        }
+    }
+}
+
+function updateScore() {
+    document.getElementById('p-score').innerHTML = pScore;
+    document.getElementById('c-score').innerHTML = cScore;
+}
+
+function checkWin() {
+    if (pScore === 5 || cScore === 5) {
+        const winner =
+            pScore === 5
+                ? 'You win the game! Congratulations!'
+                : 'Computer wins the game. Better luck next time';
+        document.getElementById('result-text').innerHTML = winner;
+        return true;
+    }
+
+    return false;
 }
